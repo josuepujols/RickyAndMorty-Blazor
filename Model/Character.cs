534d74c5934624b225282fbc3893566c37b3bed5
Josue Pujols <josue_pujols@claro.com.do>
2022-04-28 22:27:58 -0400

    initial commit

---
+++ Model/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Gender { get; set; }
+        public string Species { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Services/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetCharactersAsync(string SearchTerm = "", int page = 1);
+        Task<T> GetCharacterByIdAsync(int Id);
+        IEnumerable<T> GetCharacters(string SearchTerm = "", int page = 1);
+    }
+}
